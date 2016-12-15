4a51e974f8cfff0e0c93dccbf753920e813fedd5
Unknown <darryl.chard@students.plymouth.ac.uk>
2016-12-15 15:11:35 +0000

    Added drawing of character and first enemy, attack function needs to be made

--- character.js
+++ character.js
@@ -1,49 +1,50 @@
 var canvas = document.getElementById("gameCanvas");
 if (canvas.getContext("2d")) { // Check HTML5 canvas support
-ctx = canvas.getContext("2d"); // get Canvas Context object
+  ctx = canvas.getContext("2d"); // get Canvas Context object
+  function drawCharacter() {
+    ctx.beginPath();
+    ctx.fillStyle = "bisque"; // #ffe4c4
+    ctx.arc(200, 100, 30, 0, Math.PI * 2, true); // draw circle for head
+    // (x,y) center, radius, start angle, end angle, anticlockwise
+    ctx.fill();
 
-ctx.beginPath();
-ctx.fillStyle = "bisque"; // #ffe4c4
-ctx.arc(200, 100, 30, 0, Math.PI * 2, true); // draw circle for head
-// (x,y) center, radius, start angle, end angle, anticlockwise
-ctx.fill();
+    ctx.beginPath();
+    ctx.strokeStyle = "red"; // color
+    ctx.lineWidth = 3;
+    ctx.arc(200, 100, 20, 0, Math.PI, false); // draw semicircle for smiling
+    ctx.stroke();
 
-ctx.beginPath();
-ctx.strokeStyle = "red"; // color
-ctx.lineWidth = 3;
-ctx.arc(200, 100, 20, 0, Math.PI, false); // draw semicircle for smiling
-ctx.stroke();
+    // eyes
+    ctx.beginPath();
+    ctx.fillStyle = "green"; // color
+    ctx.arc(190, 95, 3, 0, Math.PI * 2, true); // draw left eye
+    ctx.fill();
+    ctx.arc(210, 95, 3, 0, Math.PI * 2, true); // draw right eye
+    ctx.fill();
 
-// eyes
-ctx.beginPath();
-ctx.fillStyle = "green"; // color
-ctx.arc(190, 95, 3, 0, Math.PI * 2, true); // draw left eye
-ctx.fill();
-ctx.arc(210, 95, 3, 0, Math.PI * 2, true); // draw right eye
-ctx.fill();
+    // body
+    ctx.beginPath();
+    ctx.moveTo(200, 130);
+    ctx.lineTo(200, 230);
+    ctx.strokeStyle = "navy";
+    ctx.stroke();
 
-// body
-ctx.beginPath();
-ctx.moveTo(200, 130);
-ctx.lineTo(200, 230);
-ctx.strokeStyle = "navy";
-ctx.stroke();
+    // arms
+    ctx.beginPath();
+    ctx.strokeStyle = "#0000ff"; // blue
+    ctx.moveTo(200, 130);
+    ctx.lineTo(150, 180);
+    ctx.moveTo(200, 130);
+    ctx.lineTo(250, 180);
+    ctx.stroke();
 
-// arms
-ctx.beginPath();
-ctx.strokeStyle = "#0000ff"; // blue
-ctx.moveTo(200, 130);
-ctx.lineTo(150, 180);
-ctx.moveTo(200, 130);
-ctx.lineTo(250, 180);
-ctx.stroke();
-
-// legs
-ctx.beginPath();
-ctx.strokeStyle = "orange";
-ctx.moveTo(200, 230);
-ctx.lineTo(150, 330);
-ctx.moveTo(200, 230);
-ctx.lineTo(250, 330);
-ctx.stroke();
+    // legs
+    ctx.beginPath();
+    ctx.strokeStyle = "orange";
+    ctx.moveTo(200, 230);
+    ctx.lineTo(150, 330);
+    ctx.moveTo(200, 230);
+    ctx.lineTo(250, 330);
+    ctx.stroke();
+  }
 }
--- characterEnemy.js
+++ characterEnemy.js
@@ -1,49 +1,51 @@
 var canvas = document.getElementById("gameCanvas");
 if (canvas.getContext("2d")) { // Check HTML5 canvas support
-ctx = canvas.getContext("2d"); // get Canvas Context object
+  ctx = canvas.getContext("2d"); // get Canvas Context object
+  function drawZombie() {
+    ctx.beginPath();
+    ctx.fillStyle = "green"; // #ffe4c4
+    ctx.arc(700, 100, 30, 0, Math.PI * 2, true); // draw circle for head
+    // (x,y) center, radius, start angle, end angle, anticlockwise
+    ctx.fill();
 
-ctx.beginPath();
-ctx.fillStyle = "bisque"; // #ffe4c4
-ctx.arc(700, 100, 30, 0, Math.PI * 2, true); // draw circle for head
-// (x,y) center, radius, start angle, end angle, anticlockwise
-ctx.fill();
+    ctx.beginPath();
+    ctx.strokeStyle = "red"; // color
+    ctx.lineWidth = 3;
+    ctx.arc(700, 120, 10, 0, Math.PI, true); // draw semicircle for smiling
+    ctx.stroke();
 
-ctx.beginPath();
-ctx.strokeStyle = "red"; // color
-ctx.lineWidth = 3;
-ctx.arc(700, 100, 20, 0, Math.PI, false); // draw semicircle for smiling
-ctx.stroke();
+    // eyes
+    ctx.beginPath();
+    ctx.fillStyle = "red"; // color
+    ctx.arc(690, 95, 3, 0, Math.PI * 2, true); // draw left eye
+    ctx.fill();
+    ctx.arc(710, 95, 3, 0, Math.PI * 2, true); // draw right eye
+    ctx.fill();
 
-// eyes
-ctx.beginPath();
-ctx.fillStyle = "green"; // color
-ctx.arc(690, 95, 3, 0, Math.PI * 2, true); // draw left eye
-ctx.fill();
-ctx.arc(710, 95, 3, 0, Math.PI * 2, true); // draw right eye
-ctx.fill();
+    // body
+    ctx.beginPath();
+    ctx.moveTo(700, 130);
+    ctx.lineTo(700, 230);
+    ctx.strokeStyle = "green";
+    ctx.stroke();
 
-// body
-ctx.beginPath();
-ctx.moveTo(700, 130);
-ctx.lineTo(700, 230);
-ctx.strokeStyle = "navy";
-ctx.stroke();
-
-// arms
-ctx.beginPath();
-ctx.strokeStyle = "#0000ff"; // blue
-ctx.moveTo(700, 130);
-ctx.lineTo(650, 180);
-ctx.moveTo(700, 130);
-ctx.lineTo(750, 180);
-ctx.stroke();
-
-// legs
-ctx.beginPath();
-ctx.strokeStyle = "orange";
-ctx.moveTo(700, 230);ctx
-ctx.lineTo(650, 330);
-ctx.moveTo(700, 230);
-ctx.lineTo(750, 330);
-ctx.stroke();
+    // arms
+    ctx.beginPath();
+    ctx.strokeStyle = "#green"; // blue
+    ctx.moveTo(700, 130);
+    ctx.lineTo(650, 180);
+    ctx.moveTo(700, 130);
+    ctx.lineTo(750, 180);
+    ctx.stroke();
+    
+    // legs
+    ctx.beginPath();
+    ctx.strokeStyle = "green";
+    ctx.moveTo(700, 230);ctx
+    ctx.lineTo(650, 330);
+    ctx.moveTo(700, 230);
+    ctx.lineTo(750, 330);
+    ctx.stroke();
+  }
+  
 }
